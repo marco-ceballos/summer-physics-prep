2067b26f621cdd5198e053f29e26cfaedd403996
marco-ceballos <marcoacs1809@gmail.com>
2026-05-23 11:19:02 -0700

    Added cross product, polished.py

--- Mechanics/Vectors.py
+++ Mechanics/Vectors.py
@@ -1,4 +1,3 @@
-import numpy as np
 #Calculator
 print("Welcome, please enter the values of your two vectors ")
 i1 = float(input("Value for i1: "))
@@ -7,7 +6,7 @@ k1 = float(input("Value for k1: "))
 i2 = float(input("Value for i2: "))
 j2 = float(input("Value for j2: "))
 k2 = float(input("Value for k2: "))
-act = int(input("What do you want to do? (1 for sum, 2 for sub, 3 for mult): "))
+act = int(input("What do you want to do? (1 for sum, 2 for sub, 3 for mult, 4 for cross): "))
 A = np.array([i1, j1, k1])
 B = np.array([i2, j2, k2])
 if act == 1:
@@ -15,9 +14,13 @@ if act == 1:
 elif act == 2:
   C = A - B
 elif act == 3:
-  C = A * B
+  C = np.dot(A, B)
+elif act == 4:
+  C = np.cross(A, B)
 else:
   print("Invalid input")
-print(C)
-result = np.linalg.norm(C)
-print(result)
+if act in [1, 2, 4]:
+  print("Resultant vector = ", C)
+  print("resultant magnitude = ", np.linalg.norm(C))
+elif act == 3:
+  print("Resultant scalar = ", C)
