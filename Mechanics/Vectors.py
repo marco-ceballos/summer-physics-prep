79e7bd692e779c52f233b610bbe9d96e5e9db54e
marco-ceballos <marcoacs1809@gmail.com>
2026-05-23 10:45:08 -0700

    Vector calculator for sum, sub and mult.py

---
+++ Mechanics/Vectors.py
@@ -0,0 +1,23 @@
+import numpy as np
+#Calculator
+print("Welcome, please enter the values of your two vectors ")
+i1 = float(input("Value for i1: "))
+j1 = float(input("Value for j1: "))
+k1 = float(input("Value for k1: "))
+i2 = float(input("Value for i2: "))
+j2 = float(input("Value for j2: "))
+k2 = float(input("Value for k2: "))
+act = int(input("What do you want to do? (1 for sum, 2 for sub, 3 for mult): "))
+A = np.array([i1, j1, k1])
+B = np.array([i2, j2, k2])
+if act == 1:
+  C = A + B
+elif act == 2:
+  C = A - B
+elif act == 3:
+  C = A * B
+else:
+  print("Invalid input")
+print(C)
+result = np.linalg.norm(C)
+print(result)
